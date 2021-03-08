b8d3180f8c2e330324bd44913dfee13541dfa1a5
zahidsheikh <zahidsheikhuap96@gmail.com>
2021-03-08 12:43:07 +0600

    .

---
+++ WebApplication5/WebApplication5/EmployeeDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace WebApplication5
+{
+    public class EmployeeDBContext: DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
